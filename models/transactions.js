d05c8b994dd0d8ec611715a0b44fa8571f19557e
Monir Hossen <monirhossen@pursuit.org>
2023-07-16 13:31:46 -0400

    spelling fixed

--- models/transactions.js
+++ models/transactions.js
@@ -4,56 +4,56 @@ module.exports =[
     name: "Mini",
     amount: 299,
     from: "Facebook",
-    catagory: "Income",
+    category: "Income",
     date: "07/11/2023"
 },
 {
     name: "Bini",
     amount: 199,
     from: "Zelle App",
-    catagory: "Zelle",
+    category: "Zelle",
     date: "06/11/2023"   
 },
 {
     name: "Tini",
     amount: 99,
     from: "Google",
-    catagory:"Income",
+    category:"Income",
     date:"05/11/2023"
 },
 {
     name: "Roonie",
     amount: 9,
     from: "NYU",
-    catagory:"Income",
+    category:"Income",
     date:"04/11/2023"
 },
 {
     name: "Bunny",
     amount: -199,
     from: "Pursuit",
-    catagory:"Tuition",
+    category:"Tuition",
     date:"03/11/2023"
 },
 {
     name: "Honey",
     amount: -99,
     from: "Sunocco",
-    catagory:"Gas",
+    category:"Gas",
     date:"02/11/2023"
 },
 {
     name: "Mannie",
     amount: -0.99,
     from: "Doller Tree",
-    catagory:"Grocery",
+    category:"Grocery",
     date:"01/11/2023"
 },
 {
     name: "Penny",
     amount: -0.01,
     from: "Bank of America",
-    catagory:"Adjustment",
+    category:"Adjustment",
     date:"02/20/2009"
 }
 ];
